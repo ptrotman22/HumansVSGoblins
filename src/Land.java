5e988b10ea25ba45cf3085dd536c2d8495e43137
Pierce Trotman <ptrotman22@gmail.com>
2020-03-12 17:01:21 -0400

    Land Created

--- src/Land.java
+++ src/Land.java
@@ -1,3 +1,22 @@
+/*
+* Grid - 4 by 6 grid
+* Random Treasure
+* */
+
+import java.util.*;
+import java.util.ArrayList;
+
 public class Land {
-    
+    int move = 0;
+
+
+    public int Grid(int move, int x, int y){
+        //Move direction
+        this.move = move;
+        Scanner input = new Scanner(System.in);
+        //ArrayList<Integer> grid = new ArrayList<>();
+        int[][] grid = new int[4][6];
+        
+        return
+    }
 }
