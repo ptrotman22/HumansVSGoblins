9e7eb45d047e6f958f4b6df66a69377c6cb8b667
Pierce Trotman <ptrotman22@gmail.com>
2020-03-17 11:39:58 -0400

    Grid Created
    Last post from Work computer for now.

--- src/Land.java
+++ src/Land.java
@@ -15,8 +15,21 @@ public class Land {
         this.move = move;
         Scanner input = new Scanner(System.in);
         //ArrayList<Integer> grid = new ArrayList<>();
-        int[][] grid = new int[4][6];
+        String [][] grid = new String[6][6];
+
+        // For loops governing movement
+        // Create border
+        for (int i = 0; i < grid.length; i++){
+            grid[i][i] = "| |"
+            grid[0][i] = "---";
+            grid[i][6] = "!";
+            grid[i][0] = "!";
+            grid[6][i] = "---";
+        }
+
         
-        return
+
+
+        return move;
     }
 }
