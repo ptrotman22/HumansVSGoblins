3e48a3889951d8eed93502715d9f1634e0f8e00a
Pierce T <ptrotman22@gmail.com>
2020-03-24 11:08:47 -0400

    Eliminated Border
    changed movement checks.

--- src/Land.java
+++ src/Land.java
@@ -29,57 +29,75 @@ public class Land {
                 count++;
             }
         }
-        // Create border
-        for (int i = 0; i < grid[0].length; i++) {
-            grid[0][i] = "---";
-            grid[i][6] = "!";
-            grid[i][0] = "!";
-            grid[6][i] = "---";
-        }
 
         // Choose direction
         int end = 0;
+        int mistake = 0;
         while (end == 0) {
             end = 0;
+            mistake = 0;
             // North movement
             if (move == 1) {
-                if (grid[location[0]][location[1]].equals("---") || grid[location[0]][location[1]].equals("!")) {
-                    System.out.println("Sorry you are out bounds! Please choose a different location");
-                    int tryAgain = new Scanner(System.in).nextInt();
-                    move = tryAgain;
-                    break;
-                }
                 location[0] -= 1;
+                if (location[0] < 0 || location[0] > 6) {
+                    location[0] += 1;
+                    mistake++;
+                    while (mistake >= 1) {
+                        System.out.println("Sorry you are out bounds! Please choose a different location");
+                        int tryAgain = new Scanner(System.in).nextInt();
+                        move = tryAgain;
+                        end = 1;
+                        break;
+                    }
+                }
             }
             //South Movement
             if (move == 2) {
-                if (grid[location[0]][location[1]].equals("---") || grid[location[0]][location[1]].equals("!")) {
-                    System.out.println("Sorry you are out bounds! Please choose a different location");
-                    int tryAgain = new Scanner(System.in).nextInt();
-                    move = tryAgain;
-                    break;
-                }
                 location[0] += 1;
+                if (location[0] < 0 || location[0] > 6) {
+                    location[0] -= 1;
+                    mistake++;
+                    while (mistake >= 1) {
+                        System.out.println("Sorry you are out bounds! Please choose a different location");
+                        int tryAgain = new Scanner(System.in).nextInt();
+                        move = tryAgain;
+                        end = 1;
+                        break;
+                    }
+                }
+
             }
             // West Movement
             if (move == 3) {
-                if (grid[location[0]][location[1]].equals("---") || grid[location[0]][location[1]].equals("!")) {
-                    System.out.println("Sorry you are out bounds! Please choose a different location");
-                    int tryAgain = new Scanner(System.in).nextInt();
-                    move = tryAgain;
-                    break;
-                }
                 location[1] -= 1;
+                if (location[1] < 0 || location[1] > 6) {
+                    location[1] += 1;
+                    mistake++;
+                    while (mistake >= 1) {
+                        System.out.println("Sorry you are out bounds! Please choose a different location");
+                        int tryAgain = new Scanner(System.in).nextInt();
+                        move = tryAgain;
+                        end = 1;
+                        break;
+                    }
+                }
+
             }
             // East Movement
             if (move == 4) {
-                if (grid[location[0]][location[1]].equals("---") || grid[location[0]][location[1]].equals("!")) {
-                    System.out.println("Sorry you are out bounds! Please choose a different location");
-                    int tryAgain = new Scanner(System.in).nextInt();
-                    move = tryAgain;
-                    break;
-                }
                 location[1] += 1;
+                if (location[1] < 0 || location[1] > 6) {
+                    location[1] -= 1;
+                    mistake++;
+                    while (mistake >= 1) {
+                        System.out.println("Sorry you are out bounds! Please choose a different location");
+                        int tryAgain = new Scanner(System.in).nextInt();
+                        move = tryAgain;
+                        end = 1;
+                        break;
+                    }
+                }
+
             }
         }
 
